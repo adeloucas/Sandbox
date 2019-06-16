d2bc667d224cc3e0a90164e0a7176f8b168e02db
adeloucas <andrewdeloucas@gmail.com>
2019-06-16 15:45:16 -0400

    gkab issues

--- Sandbox.py
+++ Sandbox.py
@@ -7,32 +7,40 @@ from oracc_importer import FileImport
 from jsonreader import Reader
 import os
 
-## Runs  unzip function.
 
+## Runs  unzip function.
+#
 parent_directory = os.path.expanduser('~')
 #folder = os.path.join(parent_directory, 'Python', 'json-master', 'json-master')
 #target = os.path.join(parent_directory)
-
 #FI = ORACCUnzip(folder, parent_directory)
 #FI.unzip()
 
-## Directs program to look at wherever your json files are stored. Imports catalogue
-# it was directed toward and reads(, prints,) and loads it.
 
-file = os.path.join(parent_directory, 'ORACC-Files',     # location of files
-                    'rimanum',                            # corpus
+## Directs program to look at wherever your json files are stored. Imports catalogue
+#  directed toward, reads, and loads it.
+#
+file = os.path.join(parent_directory, 'ORACC-Files',     # location of files (from unzip)
+                    'cams', 'gkab',                   # corpus
                     'catalogue.json')                    # target catalogue
+
+
+## Loads corpus, looks for text file errors
+#
 FI = FileImport(file)
 print()
 FI.read_catalogue()
 FI.load_corpus()
 print()
-#FI.print_catalogue()
-#print()
+#FI.print_catalogue() # essentially useless function outside debugging
+
 
 ## Ingests texts on a corpus or single level basis from FileImport
+#
 RE = Reader(FI.filedata)
 RE.ingest_corpus()
 RE.print_toc()
-#RE.text_information('P424508')
-#RE.print_single_text('P450699')
+
+RE.text_information('P424508')
+RE.print_single_text('P424508')
+
--- jsonreader.py
+++ jsonreader.py
@@ -19,6 +19,123 @@ class Reader(object):
         self.texts = filedata['members']
         self.failed_texts = []
 
+    def __transliteration__(self):
+        """
+
+        :return:
+        """
+        section = []
+        transliteration = []
+        line = ''
+        for text in self.data['cdl'][0]['cdl']:
+            if text['node'] == 'c':
+                section.append(text['cdl'])
+        # checks if sections have sentence structure; otherwise taken as single text.
+        for sentences in section:
+            if len(sentences) > 6:
+                for node in sentences:
+                    if node['node'] == 'c':
+                        transliteration.append(line)
+                        line = node['label'] + '.'
+                        for cdl in node['cdl']:
+                            if cdl == 'd' and 'label' in cdl.keys():
+                                line = cdl['label'] + '.'
+                            elif cdl['node'] == 'l':
+                                try:
+                                    line += ' ' + cdl['frag']
+                                except KeyError:
+                                    line += ' ' + cdl['f']['form']
+            else:
+                for node in self.text:
+                    if node['node'] == 'd' and 'label' in node.keys():
+                        transliteration.append(line)
+                        line = node['label'] + '.'
+                    elif node['node'] == 'l':
+                        try:
+                            line += ' ' + node['frag']
+                        except KeyError:
+                            line += ' ' + node['f']['form']
+        transliteration.append(line)
+        self.data['transliteration'] = transliteration[1:]
+
+    def __normalization__(self):
+        """
+
+        :return:
+        """
+        section = []
+        normalization = []
+        line = ''
+        for text in self.data['cdl'][0]['cdl']:
+            if text['node'] == 'c':
+                section.append(text['cdl'])
+        # checks if sections have sentence structure; otherwise taken as single text.
+        for sentences in section:
+            if len(sentences) > 6:
+                for node in sentences:
+                    if node['node'] == 'c':
+                        normalization.append(line)
+                        line = node['label'] + '.'
+                        for cdl in node['cdl']:
+                            if cdl == 'd' and 'label' in cdl.keys():
+                                line = cdl['label'] + '.'
+                            elif cdl['node'] == 'l':
+                                try:
+                                    line += ' ' + cdl['f']['norm']
+                                except KeyError:
+                                    line += ' ' + cdl['f']['form']
+            else:
+                for node in self.text:
+                    if node['node'] == 'd' and 'label' in node.keys():
+                        normalization.append(line)
+                        line = node['label'] + '.'
+                    elif node['node'] == 'l':
+                        try:
+                            line += ' ' + node['f']['norm']
+                        except KeyError:
+                            line += ' ' + node['f']['form']
+        normalization.append(line)
+        self.data['normalization'] = normalization[1:]
+
+    def __cuneiform__(self):  # not working, same as transliteration for now
+        """
+
+        :return:
+        """
+        section = []
+        transliteration = []
+        line = ''
+        for text in self.data['cdl'][0]['cdl']:
+            if text['node'] == 'c':
+                section.append(text['cdl'])
+        # checks if sections have sentence structure; otherwise taken as single text.
+        for sentences in section:
+            if len(sentences) > 6:
+                for node in sentences:
+                    if node['node'] == 'c':
+                        transliteration.append(line)
+                        line = node['label'] + '.'
+                        for cdl in node['cdl']:
+                            if cdl == 'd' and 'label' in cdl.keys():
+                                line = cdl['label'] + '.'
+                            elif cdl['node'] == 'l':
+                                try:
+                                    line += ' ' + cdl['frag']
+                                except KeyError:
+                                    line += ' ' + cdl['f']['form']
+            else:
+                for node in self.text:
+                    if node['node'] == 'd' and 'label' in node.keys():
+                        transliteration.append(line)
+                        line = node['label'] + '.'
+                    elif node['node'] == 'l':
+                        try:
+                            line += ' ' + node['frag']
+                        except KeyError:
+                            line += ' ' + node['f']['form']
+        transliteration.append(line)
+        self.data['transliteration'] = transliteration[1:]
+
     def __ingest_text__(self, call_number):  # pylint: disable=too-many-branches
         """
         Reads cdl documentation and outputs information into sections to be printed later.
@@ -31,48 +148,9 @@ class Reader(object):
             for node in self.data['cdl'][0]['cdl']:
                 if 'cdl' in node.keys():
                     self.text = node['cdl'][0]['cdl']  # pylint: disable= attribute-defined-outside-init
-                # fixes P424508 in saas2, only edgecase.
-                elif AttributeError:
-                    self.text = self.data['cdl'][0]['cdl']
-            transliteration = []
-            line = ''
-            for node in self.text:
-                if node['node'] == 'd' and 'label' in node.keys():
-                    transliteration.append(line)
-                    line = node['label'] + '.'
-                elif node['node'] == 'l':
-                    try:
-                        line += ' ' + node['frag']
-                    except KeyError:
-                        line += ' ' + node['f']['form']
-            transliteration.append(line)
-            normalization = []
-            line = ''
-            for node in self.text:
-                if node['node'] == 'd' and 'label' in node.keys():
-                    normalization.append(line)
-                    line = node['label'] + '.'
-                elif node['node'] == 'l':
-                    if 'norm' in node['f'].keys():
-                        line += ' ' + node['f']['norm']
-                    else:
-                        line += ' ' + node['f']['form']
-            normalization.append(line)
-            lit_trans = []
-            line = ''
-            for node in self.text:
-                if node['node'] == 'd' and 'label' in node.keys():
-                    lit_trans.append(line)
-                    line = node['label'] + '.'
-                elif node['node'] == 'l':
-                    if 'sense' in node['f'].keys():
-                        line += ' ' + node['f']['sense']
-                    else:
-                        line += ' ' + node['f']['form']
-            lit_trans.append(line)
-            self.data['transliteration'] = transliteration
-            self.data.update({'normalization': normalization})
-            self.data.update({'lit_trans': lit_trans})
+            self.__transliteration__()
+            self.__normalization__()
+            self.__cuneiform__()
         else:
             self.failed_texts.append(call_number)
             print('{text} did not ingest; text either empty or missing. (Text Fail 1)'.format(text=call_number))
@@ -107,37 +185,47 @@ class Reader(object):
                              v for k, v in ind_text[1].items() if 'id_' in k)))
         print()
 
-    def print_single_text(self, call_number):
+    def text_information(self, call_number):
+        info1 = []
+        info2 = ['cdl', 'transliteration', 'normalization', 'cuneiform']  # add filters as they are made
+        if call_number in self.texts and call_number not in self.failed_texts:
+            text = self.texts[call_number]['text_file']
+            for k, v in text.items():
+                if k not in info2:
+                    info1.append(k)
+                    print('{k}: {v}'.format(k=k, v=v))
+            print('------')
+            for k, v in self.data.items():
+                if k in info2 and k != 'cdl':
+                    if k == 'transliteration':
+                        print(('{k}: {v} lines available'.format(k=k, v=len(text['transliteration']))))
+                    elif k != 'transliteration' and v == self.data['transliteration']:
+                        pass
+                        #print('{k}: {v}'.format(k=k, v='Not available'))
+                    else:
+                        print(('{k}: {v}'.format(k=k, v='available')))
+            print()
+
+    def print_single_text(self, call_number, catalog_filter=[]):
         """
         Prints and ingests only the one text.
         :param call_number: text you wish to print.
         :return:
         """
-        self.__ingest_text__(call_number)
-        print()
-        print('Transliteration:')
-        print('\n'.join(self.data['transliteration'][1:]))
-        print()
-        print('Normalization:')
-        print('\n'.join(self.data['normalization'][1:]))
-        print()
-        print('Literal Translation:')
-        print('\n'.join(self.data['lit_trans'][1:]))
-        print()
-
-    def text_information(self, call_number):
-        self.__ingest_text__(call_number)
-        if (self.data['lit_trans'] == self.data['transliteration'] and \
-                self.data['normalization'] == self.data['transliteration']):
-            print('{text}: \n {len1} lines of transliteration; '
-                  'no normalization or literal translation available.'.
-                  format(text=call_number,
-                         len1=len(self.data['transliteration'])-1,))
+        if call_number in self.texts and call_number not in self.failed_texts:
+            text = self.texts[call_number]['text_file']
+            if len(catalog_filter) > 0:  # pylint: disable=len-as-condition
+                if catalog_filter in text:
+                    if catalog_filter == 'transliteration':
+                        print('\n'.join([line for line in text[catalog_filter]]))
+                    elif text[catalog_filter] == text['transliteration']:
+                        print('Filter not available.')
+                    else:
+                        print('\n'.join([line for line in text[catalog_filter]]))
+                else:
+                    print('not a filter, use text_information for available filters.')
+                    # make section that shows which filters are available (len > 0?)
+            else:
+                print('\n'.join([line for line in text['transliteration']]))
         else:
-            print('{text}: \n'
-                  '{len1} lines of transliteration \n'
-                  '{len2} lines of normalization \n'
-                  '{len3} lines of literal translation'. \
-                  format(text=call_number, len1=len(self.data['transliteration'])-1,
-                         len2=len(self.data['normalization'])-1,
-                         len3=len(self.data['lit_trans'])-1))
+            print('Text does not exist in this corpus.')
