8381a4bdf56fc748226b2fafba521f00df478edc
Willis Monroe <willismonroe@gmail.com>
2019-08-23 19:11:36 -0700

    Added shell of ORACC_Text class

---
+++ oracc_text.py
@@ -0,0 +1,50 @@
+import json as JSON
+
+def recursive_walk(json, key):
+    if isinstance(json, dict):
+        # need to check for 'node': 'd' and 'type': 'line-start' in here and yield those
+        for k, v in json.items():
+            if k == key:
+                yield v
+            else:
+                yield from recursive_walk(v, key)
+    elif isinstance(json, list):
+        for item in json:
+            yield from recursive_walk(item, key)
+
+
+class FileReader():
+    '''
+    This class reads in a file object and converts it from json into a native python object.
+    It should be identical in functionality to an API reader.
+    '''
+    def __init__(self, filename):
+        self.filename = filename
+        with open(self.filename) as f:
+            self.data = JSON.loads(f.read())
+
+
+class APIReader():
+    '''
+    This class reads in a URL json file from the ORACC API and turns it into a native python object.
+    It should be identical in functionality to the file reader.
+    The ORACC API is currently non-functional.
+    '''
+    def __init__(self, url):
+        self.url = url
+        # request and process url
+
+
+class ORACC_Text():
+    '''
+    This class represent a text from an ORACC corpus.
+    '''
+    def __init__(self, json):
+        self.json = json
+
+    def get_norm(self):
+        if self.norm:
+            return self.norm
+        else:
+            self.norm = [word for word in recursive_walk(self.json, 'norm')]
+            return self.norm
