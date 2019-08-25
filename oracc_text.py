944d5c93402dd2280123693cbd1d01c2a8b745c8
Willis Monroe <willismonroe@gmail.com>
2019-08-25 12:40:28 -0700

    Cleaned up Text class and added gitignore

--- oracc_text.py
+++ oracc_text.py
@@ -1,52 +1,82 @@
 import json as JSON
 
-def recursive_walk(json, key):
-    if isinstance(json, dict):
-        # need to check for 'node': 'd' and 'type': 'line-start' in here and yield those
-        # I don't think we split here... we should check if we need line breaks and pass off
-        # to another function.
-        for k, v in json.items():
-            if k == key:
-                yield v
-            else:
-                yield from recursive_walk(v, key)
-    elif isinstance(json, list):
-        for item in json:
-            yield from recursive_walk(item, key)
+
+def grab_all(input_json, type, split_lines=False):
+    def recursive_walk(json, type):
+        if isinstance(json, dict):
+            if split_lines:
+                if json.get("node") == "d":
+                    if json.get("type") == "line-start":
+                        yield f"**{json.get('label')}"
+            for k, v in json.items():
+                if k == type:
+                    yield v
+                else:
+                    yield from recursive_walk(v, type)
+        elif isinstance(json, list):
+            for item in json:
+                yield from recursive_walk(item, type)
+
+    output = [token for token in recursive_walk(input_json, type)]
+    if split_lines:
+        output = " ".join(output).split("**")
+    return output
 
 
-class FileReader():
-    '''
+class FileReader:
+    """
     This class reads in a file object and converts it from json into a native python object.
     It should be identical in functionality to an API reader.
-    '''
+    """
+
     def __init__(self, filename):
         self.filename = filename
         with open(self.filename) as f:
             self.data = JSON.loads(f.read())
 
 
-class APIReader():
-    '''
+class APIReader:
+    """
     This class reads in a URL json file from the ORACC API and turns it into a native python object.
     It should be identical in functionality to the file reader.
     The ORACC API is currently non-functional.
-    '''
+    """
+
     def __init__(self, url):
         self.url = url
-        # request and process url
+        # TODO: request and process url
 
 
-class ORACC_Text():
-    '''
+class ORACC_Text:
+    """
     This class represent a text from an ORACC corpus.
-    '''
+    """
+
     def __init__(self, json):
         self.json = json
+        self.norm = None
+        self.translit = None
 
     def get_norm(self):
         if self.norm:
             return self.norm
         else:
-            self.norm = [word for word in recursive_walk(self.json, 'norm')]
+            self.norm = grab_all(self.json, type="norm")
             return self.norm
+
+    def pprint_norm(self):
+        for line in grab_all(self.json, "norm", split_lines=True):
+            print(line)
+
+    def get_translit(self):
+        if self.translit:
+            return self.translit
+        else:
+            self.translit = grab_all(self.json, type="frag")
+            return self.translit
+
+    def pprint_translit(self):
+        for line in grab_all(self.json, "frag", split_lines=True):
+            print(line)
+
+    # TODO: add 'sense'
