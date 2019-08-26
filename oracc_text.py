9f0751d2817620bcc8904d054355036699c63d54
Willis Monroe <willismonroe@gmail.com>
2019-08-26 07:43:46 -0700

    Added type hinting for fun.

--- oracc_text.py
+++ oracc_text.py
@@ -1,12 +1,12 @@
 import json as JSON
-
+from typing import Dict, List, Any
 
 import requests
 from bs4 import BeautifulSoup
 
 
-def grab_translation(project, pnum):
-    translation = []
+def grab_translation(project: str, pnum: str):
+    translation: List[str] = []
     r = requests.get(f"http://oracc.org/{project}/{pnum}")
     soup = BeautifulSoup(r.text)
     for line in soup.find_all("p", class_="tr"):
@@ -20,7 +20,7 @@ def grab_translation(project, pnum):
     return translation
 
 
-def grab_all(input_json, type, split_lines=False):
+def grab_all(input_json, type: str, split_lines: bool = False) -> List[str]:
     def recursive_walk(json, type):
         if isinstance(json, dict):
             if split_lines:
@@ -36,7 +36,7 @@ def grab_all(input_json, type, split_lines=False):
             for item in json:
                 yield from recursive_walk(item, type)
 
-    output = [token for token in recursive_walk(input_json, type)]
+    output: list = [token for token in recursive_walk(input_json, type)]
     if split_lines:
         output = " ".join(output).split("**")
     return output
@@ -49,10 +49,10 @@ class FileReader:
     It should be identical in functionality to an API reader.
     """
 
-    def __init__(self, filename):
+    def __init__(self, filename: str):
         self.filename = filename
         with open(self.filename) as f:
-            self.data = JSON.loads(f.read())
+            self.data: Dict[str, Any] = JSON.loads(f.read())
 
 
 class APIReader:
@@ -63,7 +63,7 @@ class APIReader:
     The ORACC API is currently non-functional.
     """
 
-    def __init__(self, url):
+    def __init__(self, url: str):
         self.url = url
         # TODO: request and process url
 
@@ -73,32 +73,32 @@ class ORACC_Text:
     This class represent a text from an ORACC corpus.
     """
 
-    def __init__(self, json):
-        self.json = json
-        self.norm = None
-        self.translit = None
+    def __init__(self, json: dict):
+        self.json: Dict[str, Any] = json
+        self.norm: List[str] = []
+        self.translit: List[str] = []
 
-    def get_norm(self):
+    def get_norm(self) -> List[str]:
         # I'm not sure of these if/else statements actually do anything with
         # such small texts.
-        if self.norm:
+        if len(self.norm) > 0:
             return self.norm
         else:
             self.norm = grab_all(self.json, type="norm")
             return self.norm
 
-    def pprint_norm(self):
+    def pprint_norm(self) -> None:
         for line in grab_all(self.json, "norm", split_lines=True):
             print(line)
 
-    def get_translit(self):
-        if self.translit:
+    def get_translit(self) -> List[str]:
+        if len(self.translit) > 0:
             return self.translit
         else:
             self.translit = grab_all(self.json, type="frag")
             return self.translit
 
-    def pprint_translit(self):
+    def pprint_translit(self) -> None:
         for line in grab_all(self.json, "frag", split_lines=True):
             print(line)
 
